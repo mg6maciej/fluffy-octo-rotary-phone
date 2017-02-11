eab18c83f32b178fcf72eec31d5c3426f9e820f0
Maciej Górski <maciek.gorski@gmail.com>
2017-02-11 13:55:59 +0100

    Improve code coverage by opening empty screen.

---
+++ app/src/androidTest/java/pl/mg6/likeornot/GridLoadTest.kt
@@ -0,0 +1,15 @@
+package pl.mg6.likeornot
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class GridLoadTest {
+
+    @Rule @JvmField
+    val rule = ActivityTestRule<GridActivity>(GridActivity::class.java)
+
+    @Test
+    fun nothing() {
+    }
+}
