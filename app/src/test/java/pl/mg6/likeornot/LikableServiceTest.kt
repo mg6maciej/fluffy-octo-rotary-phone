06d6eac20e3efca4c555616b2c8b9ec92b2bff42
Maciej Górski <maciek.gorski@gmail.com>
2017-02-11 15:55:05 +0100

    Use mockito-kotlin in unit tests.

--- app/src/test/java/pl/mg6/likeornot/LikableServiceTest.kt
+++ app/src/test/java/pl/mg6/likeornot/LikableServiceTest.kt
@@ -1,30 +1,28 @@
 package pl.mg6.likeornot
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Single.just
 import org.junit.Test
 
 class LikableServiceTest {
 
+    private val api = mock<LikableApi>()
+
     @Test
     fun shouldReturnEmptyList() {
-        val api = object : LikableApi {
-            override fun call() = just(emptyList<LikableFromApi>())
-        }
-        val observer = getLikables(api).test()
-        observer.assertValue(emptyList())
+        whenever(api.call()).thenReturn(just(emptyList()))
+        getLikables(api).test().assertValue(emptyList())
     }
 
     @Test
     fun shouldReturnListWithLikable() {
-        val api = object : LikableApi {
-            override fun call() = just(listOf(LikableFromApi(
-                    uuid = "ba43bbb1-a904-11e1-9412-005056900141",
-                    name = "Michael Jackson",
-                    image = listOf("https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200")
-            )))
-        }
-        val observer = getLikables(api).test()
-        observer.assertValue(listOf(Likable(
+        whenever(api.call()).thenReturn(just(listOf(LikableFromApi(
+                uuid = "ba43bbb1-a904-11e1-9412-005056900141",
+                name = "Michael Jackson",
+                image = listOf("https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200")
+        ))))
+        getLikables(api).test().assertValue(listOf(Likable(
                 uuid = "ba43bbb1-a904-11e1-9412-005056900141",
                 name = "Michael Jackson",
                 image = "https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200"
