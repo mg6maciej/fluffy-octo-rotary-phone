7f20c2f3f6a86453eb196918f4e9317a0b2ab160
Maciej Górski <maciek.gorski@gmail.com>
2017-02-11 13:26:19 +0100

    Add empty grid screen.

---
+++ app/src/main/java/pl/mg6/likeornot/GridActivity.kt
@@ -0,0 +1,11 @@
+package pl.mg6.likeornot
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GridActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
