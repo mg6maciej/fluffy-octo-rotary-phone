036d9c76eaf07b56e65599b5e303410ade6fbbbc
Maciej Górski <maciek.gorski@gmail.com>
2017-02-11 15:59:48 +0100

    [refactoring] Extract stub and assert functions.

--- app/src/test/java/pl/mg6/likeornot/LikableServiceTest.kt
+++ app/src/test/java/pl/mg6/likeornot/LikableServiceTest.kt
@@ -7,25 +7,33 @@ import org.junit.Test
 
 class LikableServiceTest {
 
-    private val api = mock<LikableApi>()
+    private val api: LikableApi = mock()
 
     @Test
     fun shouldReturnEmptyList() {
-        whenever(api.call()).thenReturn(just(emptyList()))
-        getLikables(api).test().assertValue(emptyList())
+        stubApiToReturn(emptyList())
+        assertServiceReturns(emptyList())
     }
 
     @Test
     fun shouldReturnListWithLikable() {
-        whenever(api.call()).thenReturn(just(listOf(LikableFromApi(
+        stubApiToReturn(listOf(LikableFromApi(
                 uuid = "ba43bbb1-a904-11e1-9412-005056900141",
                 name = "Michael Jackson",
                 image = listOf("https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200")
-        ))))
-        getLikables(api).test().assertValue(listOf(Likable(
+        )))
+        assertServiceReturns(listOf(Likable(
                 uuid = "ba43bbb1-a904-11e1-9412-005056900141",
                 name = "Michael Jackson",
                 image = "https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200"
         )))
+    }
+
+    private fun stubApiToReturn(value: List<LikableFromApi>) {
+        whenever(api.call()).thenReturn(just(value))
+    }
+
+    private fun assertServiceReturns(expected: List<Likable>) {
+        getLikables(api).test().assertValue(expected)
     }
 }
