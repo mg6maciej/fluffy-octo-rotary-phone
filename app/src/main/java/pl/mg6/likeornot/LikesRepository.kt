acb92c314e32cafde58ea9f9e7cb6c8a2f8f6b6e
Maciej Górski <maciek.gorski@gmail.com>
2017-02-15 12:47:55 +0100

    [refactoring] Extract functions.

--- app/src/main/java/pl/mg6/likeornot/LikesRepository.kt
+++ app/src/main/java/pl/mg6/likeornot/LikesRepository.kt
@@ -6,10 +6,16 @@ import java.io.File
 
 fun callLocalLikes(context: Context): Single<Map<String, Status>> {
     val likesFile = File(context.filesDir, "likes")
-    return if (!likesFile.exists()) {
-        Single.just(emptyMap())
-    } else {
+    return if (likesFile.exists()) {
         Single.fromCallable { likesFile.readLines() }
-                .map { it.associateBy({ it.substringBefore(' ') }, { Status.valueOf(it.substringAfter(' ')) }) }
+                .map(::toUuidToStatusMap)
+    } else {
+        Single.just(emptyMap())
     }
 }
+
+private fun toUuidToStatusMap(lines: List<String>) = lines.associateBy(::extractUuid, ::extractStatus)
+
+private fun extractUuid(line: String) = line.substringBefore(' ')
+
+private fun extractStatus(line: String) = Status.valueOf(line.substringAfter(' '))
