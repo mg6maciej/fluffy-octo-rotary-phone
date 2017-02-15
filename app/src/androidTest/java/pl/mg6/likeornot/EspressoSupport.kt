b7c0ae72a27a201c3e5fed5141d856b75d9f2b78
Maciej Górski <maciek.gorski@gmail.com>
2017-02-15 21:32:17 +0100

    [refactoring] Move LikeableItem interactions to separate file.

--- app/src/androidTest/java/pl/mg6/likeornot/EspressoSupport.kt
+++ app/src/androidTest/java/pl/mg6/likeornot/EspressoSupport.kt
@@ -1,91 +1,14 @@
 package pl.mg6.likeornot
 
 import android.support.annotation.DrawableRes
-import android.support.annotation.IdRes
-import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.action.ViewActions
-import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.BoundedMatcher
-import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.ImageView
-import com.elpassion.android.commons.espresso.click
-import com.elpassion.android.commons.espresso.isNotDisplayed
-import com.elpassion.android.commons.espresso.onId
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.core.AllOf.allOf
-import org.hamcrest.core.IsNot.not
-import pl.mg6.likeornot.OnScreenMatcher.isOnScreenAtLeast
-
-data class LikableItemViewInteraction(val vi: ViewInteraction)
-
-fun onLikableItem(@IdRes id: Int): LikableItemViewInteraction {
-    return LikableItemViewInteraction(onView(allOf(withId(id), isOnScreenAtLeast(50))))
-}
-
-fun LikableItemViewInteraction.click(): LikableItemViewInteraction {
-    vi.click()
-    return this
-}
-
-fun LikableItemViewInteraction.isNotDisplayed(): LikableItemViewInteraction {
-    vi.isNotDisplayed()
-    return this
-}
-
-fun LikableItemViewInteraction.hasName(name: String): LikableItemViewInteraction {
-    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_name), withText(name)))))
-    return this
-}
-
-fun LikableItemViewInteraction.hasStatus(@DrawableRes imageId: Int): LikableItemViewInteraction {
-    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status), withImage(imageId)))))
-    return this
-}
-
-fun LikableItemViewInteraction.hasNoStatus(): LikableItemViewInteraction {
-    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status), withNoImage()))))
-    return this
-}
-
-fun LikableItemViewInteraction.hasStatusOverlay(): LikableItemViewInteraction {
-    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status_overlay), isDisplayed()))))
-    return this
-}
-
-fun LikableItemViewInteraction.doesntHaveStatusOverlay(): LikableItemViewInteraction {
-    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status_overlay), not(isDisplayed())))))
-    return this
-}
-
-fun LikableItemViewInteraction.selectReallyLike(): LikableItemViewInteraction {
-    return selectWithId(R.id.grid_overlay_really_like)
-}
-
-fun LikableItemViewInteraction.selectLike(): LikableItemViewInteraction {
-    return selectWithId(R.id.grid_overlay_like)
-}
-
-fun LikableItemViewInteraction.selectMeh(): LikableItemViewInteraction {
-    return selectWithId(R.id.grid_overlay_meh)
-}
-
-fun LikableItemViewInteraction.selectDontLike(): LikableItemViewInteraction {
-    return selectWithId(R.id.grid_overlay_dont_like)
-}
-
-fun LikableItemViewInteraction.selectHate(): LikableItemViewInteraction {
-    return selectWithId(R.id.grid_overlay_hate)
-}
-
-private fun LikableItemViewInteraction.selectWithId(viewId: Int): LikableItemViewInteraction {
-    vi.click()
-    onId(viewId).click()
-    return this
-}
 
 fun ViewInteraction.swipeLeft(): ViewInteraction {
     return perform(ViewActions.swipeLeft())
@@ -95,7 +18,7 @@ fun ViewInteraction.swipeRight(): ViewInteraction {
     return perform(ViewActions.swipeRight())
 }
 
-private fun withImage(@DrawableRes imageId: Int): Matcher<View> {
+fun withImage(@DrawableRes imageId: Int): Matcher<View> {
     return object : BoundedMatcher<View, ImageView>(ImageView::class.java) {
 
         override fun matchesSafely(view: ImageView): Boolean {
@@ -111,7 +34,7 @@ private fun withImage(@DrawableRes imageId: Int): Matcher<View> {
     }
 }
 
-private fun withNoImage(): Matcher<View> {
+fun withNoImage(): Matcher<View> {
     return object : BoundedMatcher<View, ImageView>(ImageView::class.java) {
 
         override fun matchesSafely(view: ImageView): Boolean {
---
+++ app/src/androidTest/java/pl/mg6/likeornot/LikableItemViewInteractions.kt
@@ -0,0 +1,81 @@
+package pl.mg6.likeornot
+
+import android.support.annotation.DrawableRes
+import android.support.annotation.IdRes
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import com.elpassion.android.commons.espresso.click
+import com.elpassion.android.commons.espresso.isNotDisplayed
+import com.elpassion.android.commons.espresso.onId
+import org.hamcrest.core.AllOf.allOf
+import org.hamcrest.core.IsNot
+import pl.mg6.likeornot.OnScreenMatcher.isOnScreenAtLeast
+
+data class LikableItemViewInteraction(val vi: ViewInteraction)
+
+fun onLikableItem(@IdRes id: Int): LikableItemViewInteraction {
+    return LikableItemViewInteraction(onView(allOf(withId(id), isOnScreenAtLeast(50))))
+}
+
+fun LikableItemViewInteraction.click(): LikableItemViewInteraction {
+    vi.click()
+    return this
+}
+
+fun LikableItemViewInteraction.isNotDisplayed(): LikableItemViewInteraction {
+    vi.isNotDisplayed()
+    return this
+}
+
+fun LikableItemViewInteraction.hasName(name: String): LikableItemViewInteraction {
+    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_name), withText(name)))))
+    return this
+}
+
+fun LikableItemViewInteraction.hasStatus(@DrawableRes imageId: Int): LikableItemViewInteraction {
+    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status), withImage(imageId)))))
+    return this
+}
+
+fun LikableItemViewInteraction.hasNoStatus(): LikableItemViewInteraction {
+    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status), withNoImage()))))
+    return this
+}
+
+fun LikableItemViewInteraction.hasStatusOverlay(): LikableItemViewInteraction {
+    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status_overlay), isDisplayed()))))
+    return this
+}
+
+fun LikableItemViewInteraction.doesntHaveStatusOverlay(): LikableItemViewInteraction {
+    vi.check(matches(hasDescendant(allOf(withId(R.id.likable_item_status_overlay), IsNot.not(isDisplayed())))))
+    return this
+}
+
+fun LikableItemViewInteraction.selectReallyLike(): LikableItemViewInteraction {
+    return selectWithId(R.id.grid_overlay_really_like)
+}
+
+fun LikableItemViewInteraction.selectLike(): LikableItemViewInteraction {
+    return selectWithId(R.id.grid_overlay_like)
+}
+
+fun LikableItemViewInteraction.selectMeh(): LikableItemViewInteraction {
+    return selectWithId(R.id.grid_overlay_meh)
+}
+
+fun LikableItemViewInteraction.selectDontLike(): LikableItemViewInteraction {
+    return selectWithId(R.id.grid_overlay_dont_like)
+}
+
+fun LikableItemViewInteraction.selectHate(): LikableItemViewInteraction {
+    return selectWithId(R.id.grid_overlay_hate)
+}
+
+private fun LikableItemViewInteraction.selectWithId(viewId: Int): LikableItemViewInteraction {
+    vi.click()
+    onId(viewId).click()
+    return this
+}
