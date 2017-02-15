90d7e33e3d43756f04c5dadae40a528632993352
Maciej Górski <maciek.gorski@gmail.com>
2017-02-15 22:19:35 +0100

    Remove default value from likable status.

--- app/src/main/java/pl/mg6/likeornot/Likable.kt
+++ app/src/main/java/pl/mg6/likeornot/Likable.kt
@@ -4,4 +4,4 @@ data class Likable(
         val uuid: String,
         val name: String,
         val image: String?,
-        var status: Status? = null)
+        var status: Status?)
--- app/src/testCommon/java/pl/mg6/likeornot/LikableFactory.kt
+++ app/src/testCommon/java/pl/mg6/likeornot/LikableFactory.kt
@@ -4,6 +4,7 @@ val michaelJacksonLikable by lazy {
     Likable(
             uuid = "ba43bbb1-a904-11e1-9412-005056900141",
             name = "Michael Jackson",
-            image = "https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200"
+            image = "https://d93golxnkabrk.cloudfront.net/things/ba43bbb1-a904-11e1-9412-005056900141.jpg?w=200",
+            status = null
     )
 }
