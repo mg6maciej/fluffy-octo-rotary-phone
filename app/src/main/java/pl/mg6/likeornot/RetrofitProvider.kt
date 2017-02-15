2453914fd75b13504ca31a3bc23a1de2d4d15fa7
Maciej Górski <maciek.gorski@gmail.com>
2017-02-15 20:00:21 +0100

    Make createRetrofit return type explicit.

--- app/src/main/java/pl/mg6/likeornot/RetrofitProvider.kt
+++ app/src/main/java/pl/mg6/likeornot/RetrofitProvider.kt
@@ -7,10 +7,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitProvider {
 
-    private val retrofit: Retrofit by lazy(this::createRetrofit)
+    private val retrofit by lazy(this::createRetrofit)
     var override: (() -> Nothing)? = null
 
-    private fun createRetrofit() = Retrofit.Builder()
+    private fun createRetrofit(): Retrofit = Retrofit.Builder()
             .baseUrl("https://raw.githubusercontent.com/mg6maciej/fluffy-octo-rotary-phone/master/")
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .addConverterFactory(GsonConverterFactory.create())
