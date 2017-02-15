7b3a924ef76b5775a3c40d43c2c141f7c1a12934
Maciej Górski <maciek.gorski@gmail.com>
2017-02-15 13:43:20 +0100

    [refactoring] Extract local storage related tests into separate class.

--- app/src/androidTest/java/pl/mg6/likeornot/GridLocalStorageTest.kt
+++ app/src/androidTest/java/pl/mg6/likeornot/GridLocalStorageTest.kt
@@ -7,21 +7,29 @@ import org.junit.Rule
 import org.junit.Test
 import java.io.File
 
-class GridLoadMultipleTest {
+class GridLocalStorageTest {
 
     @Rule @JvmField
     val rule = object : ActivityTestRule<GridActivity>(GridActivity::class.java) {
+
+        private val likesFile by lazy { File(getTargetContext().filesDir, "likes") }
 
         override fun beforeActivityLaunched() {
             LikableApiProvider.override = object : LikableApi {
                 override fun call() = just(listOf(michaelJacksonLikableFromApi, wayneRooneyLikableFromApi))
             }
+            likesFile.writeText(
+                    "${michaelJacksonLikableFromApi.uuid} LIKE\n"
+            )
+        }
+
+        override fun afterActivityFinished() {
+            likesFile.delete()
         }
     }
 
     @Test
-    fun shouldShowLikables() {
-        onLikableItem(R.id.likable_item_1).hasName("Michael Jackson")
-        onLikableItem(R.id.likable_item_2).hasName("Wayne Rooney")
+    fun shouldShowInitialLike() {
+        onLikableItem(R.id.likable_item_1).hasStatus(R.drawable.like)
     }
 }
