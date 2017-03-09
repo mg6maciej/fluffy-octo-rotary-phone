5d27f199758f3d2bc387be5a9a1af123d1ba0cf9
Maciej Górski <maciek.gorski@gmail.com>
2017-03-09 15:20:12 +0100

    Add LeakCanary.

--- app/src/main/java/pl/mg6/likeornot/LikeOrNotApp.kt
+++ app/src/main/java/pl/mg6/likeornot/LikeOrNotApp.kt
@@ -2,12 +2,17 @@ package pl.mg6.likeornot
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.squareup.leakcanary.LeakCanary
 import io.fabric.sdk.android.Fabric
 
 class LikeOrNotApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
         Fabric.with(this, Crashlytics())
     }
 }
