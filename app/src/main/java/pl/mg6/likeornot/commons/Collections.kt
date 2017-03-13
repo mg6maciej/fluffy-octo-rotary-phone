8a0dc8cc62d47a295b784e2c36b8f1407c07db0a
Maciej Górski <maciek.gorski@gmail.com>
2017-03-13 12:14:10 +0100

    [refactoring] Rename to fromIndex, extract toIndex.

--- app/src/main/java/pl/mg6/likeornot/commons/Collections.kt
+++ app/src/main/java/pl/mg6/likeornot/commons/Collections.kt
@@ -4,8 +4,9 @@ fun <T> List<T>.chunk(chunkSize: Int): List<List<T>> {
     require(chunkSize > 0) { "chunkSize must be greater than 0" }
     val chunksCount = (size + chunkSize - 1) / chunkSize
     val chunkedList = ArrayList<List<T>>(chunksCount)
-    for (i in 0 until size step chunkSize) {
-        chunkedList.add(subList(i, minOf(i + chunkSize, size)))
+    for (fromIndex in 0 until size step chunkSize) {
+        val toIndex = minOf(fromIndex + chunkSize, size)
+        chunkedList.add(subList(fromIndex, toIndex))
     }
     return chunkedList
 }
